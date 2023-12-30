ddcf5a5dc92e5857a311f7421a9ac5c9d36cded9
saravanan m <20110086@hicet.ac.in>
2023-12-30 22:33:59 +0530

    commit

---
+++ function.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$("#aboutcontent").mouseover(function(){
+    $("#about-content").fadeIn(3000)
+})
+$("#skillcontent").mouseover(function(){
+    $("#imgcontent").slideDown(3000)
+})
+});
